366cfd5d9299c9539ece6d0eb27a4c4c2e4309b2
Move Up Bangladesh PLC <157596809+moveupbd@users.noreply.github.com>
2024-03-28 11:12:37 +0600

    Update urlpattern media and static on urls.py

--- src/urls.py
+++ src/urls.py
@@ -24,4 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", hello_world, name='hello_world'),
     path('api/', include('members.urls')),
-]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
